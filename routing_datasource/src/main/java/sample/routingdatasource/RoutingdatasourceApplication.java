5f03a16d1556c1a5acd96e05dcbc5bff17c3f5c4
Akihiro Yamada <u.parr917@gmail.com>
2023-08-14 14:55:12 +0900

    動的にdata sourceを切り替えるサンプル

---
+++ routing_datasource/src/main/java/sample/routingdatasource/RoutingdatasourceApplication.java
@@ -0,0 +1,13 @@
+package sample.routingdatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoutingdatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoutingdatasourceApplication.class, args);
+	}
+
+}
